0d801d251cabbb2345afca0f623cf21cf0f13dd8
sarahwonago <sarahshaaleh@gmail.com>
2024-07-07 09:49:25 +0300

    securing environment variables

--- devpro/urls.py
+++ devpro/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("accounts/", include('django.contrib.auth.urls')),
+    
     path("accounts/", include('users.urls')),
     path("", include('core.urls')),
     
